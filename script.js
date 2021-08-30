b0dbd3676de46f244ebadc6b58a5f40baafc569b
Carasan-byte <siatcarasan@gmail.com>
2021-08-30 18:59:43 +0200

    background change and  h1 and p tags

---
+++ script.js
@@ -0,0 +1,7 @@
+let data = {
+    photo: 'images/YEuJOxz.jpeg',
+    title: 'The Outlaw Josey Wales',
+    description: 'Josey Wales egyszerű farmerként élt családjával, távol a világ folyásától, saját tanyáján.Ám amikor a polgárháború szele végigsöpört Amerikán, Josey- t sem kímélte: családját meggyilkolták, farmját felégették az épphogy átvonuló, válogatás nélkül pusztító csapatok.A békés farmer beállt a környék hírhedt banditái közé, hogy megkeresse azokat, akik tönkretették az életét.'
+};
+
+$('#photo').attr('images/YEuJOxz.jpeg', data.photo);
